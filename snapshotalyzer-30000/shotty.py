f449bd39c0347a44fdf121b2cc487c61da0284b2
jsm2000 <jsm2000@hotmail.com>
2020-07-15 10:35:54 -0400

    Handle error when stopping a pending instance or starting a stopping one.

--- snapshotalyzer-30000/shotty.py
+++ snapshotalyzer-30000/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -120,7 +121,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0} ...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0} .".format(i.id) + str(e))
+            continue #not really needed but we're being explicit here
 
     return
 
@@ -133,7 +138,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0} ...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0} .".format(i.id) + str(e))
+            continue #not really needed but we're being explicit here
 
     return
 
